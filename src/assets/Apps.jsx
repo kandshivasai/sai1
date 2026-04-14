de412cbc75f9c5f93e3d2e11155e94bb92adb603
kandshivasai <shivasaikandhi@gmail.com>
2026-04-14 14:19:17 +0530

    first commit

---
+++ src/assets/Apps.jsx
@@ -0,0 +1,24 @@
+ export default function Apps(plate){
+    return(
+        <>
+        
+    <div style={{ height:"380px",width:"260px" ,borderRadius:"10px", backgroundColor:"lightpink",textAlign:"center",paddingTop:"40px",}}>  
+      <img src={plate} alt="" style={{
+            width: "150px",
+            height: "150px",
+            borderRadius: "50px", }}
+            
+            
+          />
+      <h6>Zomato</h6>
+      <p  style={{padding:"px"}}>Lorem ipsum dolor sit amet 
+        <br>
+        </br>Totam, velit est consequuntur  <br></br>
+        maxime tenetur tempore</p><br>
+        </br>
+      <button type="button" style={{backgroundColor:"pink",borderRadius:"10px"}}>submit</button>
+    </div>
+        </>
+    )
+}
+
---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Apps from './assets/Apps'
+import Heading from './assets/card'
+
+// function react1(){
+//   return "hello wirld"
+// }
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <div style={{textAlign:"center"}}>
+      <h1>enternal </h1>
+      <h4>POWERING INDIA’S
+    CHANGING LIFESTYLES</h4>
+    </div>
+    <div style={{display:"flex",justifyContent:"space-evenly"}}>
+    {Apps("https://b.zmtcdn.com/data/o2_assets/d1eee2be61cf47e2332cb7c49475c0981739777714.png")}
+    {Apps("https://b.zmtcdn.com/data/o2_assets/071cb96db84f20eea3a39804e113bdee1739777655.png")}
+    {Apps("https://b.zmtcdn.com/data/o2_assets/b750e7c0113f10cc6b3cec658c7229d71770799109.png")}
+    {Apps("https://b.zmtcdn.com/data/o2_assets/9207cd0fc68c4ac55cfd3bfa00c02a351739777699.png")}
+    
+
+    </div>
+
+  </StrictMode>,)
